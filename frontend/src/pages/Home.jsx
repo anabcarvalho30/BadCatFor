6997bf5ae1b455c53fe9a5fbd25a5ae28b68c19a
Ana <ana.menezes62@aluno.ifce.edu.br>
2026-01-03 14:45:09 -0300

    Cards

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Ghost, Gamepad2, Users, ArrowRight } from 'lucide-react';
+import { Star, Gamepad2, Users, ArrowRight } from 'lucide-react';
 
 const Home = () => {
   return (
@@ -10,9 +10,9 @@ const Home = () => {
             Bem-vindo ao <span>BadCat Games</span>
           </h1>
           <p className="home-hero-subtitle">
-            O fórum oficial dos desenvolvedores de Demon's trudge, </p>
-          <p  className="home-hero-subtitle">
-            Tiny Whispers, Morris' Nights of Wonders e The Campus! </p>
+            O fórum oficial dos desenvolvedores de Demon's trudge,
+            Tiny Whispers, Morris' Nights of Wonders e The Campus!
+          </p>
           <Link to="/games" className="auth-btn home-hero-button">
             <Gamepad2 size={24} />
             Explorar Jogos
@@ -21,25 +21,24 @@ const Home = () => {
       </section>
 
       <div className="container home-features">
-        <h2 className="home-features-title">Por que entrar no BadCatFor?</h2>
-        
+        <h2 className="home-features-title">BadCat Fórum!</h2>
+
         <div className="features-grid">
-          
-          {/* Card 1 */}
+
           <div className="game-card feature-card">
-            <div className="feature-icon"><Ghost size={48} /></div>
-            <h3>Jogos Assustadores</h3>
-            <p>Uma curadoria dos melhores (e piores) jogos de terror, dos clássicos aos indies obscuros.</p>
+            <div className="feature-icon">
+              <Star size={48} fill="#ffffffff"/>
+            </div>
+            <h3>Constantes atualizações!</h3>
+            <p>Os devs são altamente ativos! Nosso objetivo é entregar os melhores e mais acessíveis jogos!</p>
           </div>
 
-          {/* Card 2 */}
           <div className="game-card feature-card">
             <div className="feature-icon"><Users size={48} /></div>
             <h3>Comunidade Ativa</h3>
             <p>Conecte-se com outros jogadores, personalize seu perfil e crie sua reputação no fórum.</p>
           </div>
 
-          {/* Card 3 */}
           <div className="game-card feature-card">
             <div className="feature-icon"><Gamepad2 size={48} /></div>
             <h3>Reviews Sinceros</h3>
@@ -64,7 +63,7 @@ const Home = () => {
               <h3>Silent Hill 2 Remake: O pesadelo retorna</h3>
               <p>Confira nossa análise completa sobre o retorno triunfante da Konami...</p>
             </div>
-            
+
             <div className="news-item">
               <small className="news-date">28 Dez, 2025</small>
               <h3>Resident Evil 9 anunciado?</h3>
